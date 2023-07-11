b80b943fb30ef346a0ae7b2a7d3c46682a7405cc
Zuby Javed <zubyjaved@gmail.com>
2023-07-11 14:06:14 -0700

    while speech is being read, change mic icon to red

--- content.js
+++ content.js
@@ -1,35 +1,41 @@
-function startSpeechRecognition() {
-    // Start capturing audio
+function startSpeechRecognition(micButton) {
     let recognition = new webkitSpeechRecognition();
     recognition.lang = 'en-US';
 
+    let oldImageUrl = micButton.style.backgroundImage;
+
+    recognition.onstart = function () {
+        let newImageUrl = chrome.runtime.getURL("src/assets/mic-icon-active.png");
+        micButton.style.backgroundImage = `url('${newImageUrl}')`;
+    }
+
     recognition.onresult = function (event) {
         let transcript = event.results[0][0].transcript;
-        document.getElementById('prompt-textarea').value += transcript;
+        document.getElementById('prompt-textarea').value = transcript;
         console.log(transcript);
+        let newImageUrl = chrome.runtime.getURL("src/assets/mic-icon.png");
+        micButton.style.backgroundImage = `url('${newImageUrl}')`;
     }
+
+    recognition.onend = function () {
+        micButton.style.backgroundColor = 'transparent';
+    }
+
     recognition.start();
 }
 
 function createMicButton() {
     let micButton = document.createElement('button');
-    let imageUrl = chrome.runtime.getURL("mic-icon.png");
+    let imageUrl = chrome.runtime.getURL("src/assets/mic-icon.png");
     micButton.style.backgroundImage = `url('${imageUrl}')`;
     micButton.style.backgroundRepeat = 'no-repeat';
     micButton.style.backgroundSize = 'contain';
     micButton.style.backgroundPosition = 'center';
-    micButton.style.borderRadius = '100%';
+    micButton.style.width = '15px';
+    micButton.style.height = '15px';
     micButton.style.padding = '10px';
     micButton.style.marginRight = '15px';
-
-    micButton.onmouseover = () => {
-        micButton.style.backgroundColor = 'grey';
-    };
-    micButton.onmouseout = () => {
-        micButton.style.backgroundColor = 'transparent';
-    };
-    micButton.onclick = startSpeechRecognition;
-
+    micButton.onclick = () => startSpeechRecognition(micButton);  // Pass micButton as argument
     return micButton;
 }
 
@@ -37,19 +43,15 @@ setTimeout(() => {
     let textArea = document.getElementById('prompt-textarea');
     let parentElement = textArea.parentElement;
 
-    // Create a wrapper div
     let wrapperDiv = document.createElement('div');
     wrapperDiv.style.display = 'flex';
     wrapperDiv.style.alignItems = 'center';
 
-    // Create the mic button
     const micButton = createMicButton();
 
-    // Remove the textarea from its parent and append it to the wrapper div
     parentElement.removeChild(textArea);
     wrapperDiv.appendChild(micButton);
     wrapperDiv.appendChild(textArea);
 
-    // Add the wrapper div to the original parent of the textarea
     parentElement.appendChild(wrapperDiv);
 }, 2000);
