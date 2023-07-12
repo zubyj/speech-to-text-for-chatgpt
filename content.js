03be8fafb2aae29605a0082b0b55ce91a4dc35c3
Zuby Javed <zubyjaved@gmail.com>
2023-07-11 17:25:06 -0700

    avoid creating new mics when the page is dynamically updated

--- content.js
+++ content.js
@@ -13,6 +13,15 @@ function startSpeechRecognition(micButton) {
         textArea.value = transcript;
         let newImageUrl = chrome.runtime.getURL("src/assets/mic-icon.png");
         micButton.style.backgroundImage = `url('${newImageUrl}')`;
+
+        // Create new 'input' event
+        let e = new Event('input', {
+            bubbles: true,
+            cancelable: true,
+        });
+
+        // Dispatch it on the textarea
+        textArea.dispatchEvent(e);
     }
     recognition.start();
 }
@@ -20,6 +29,7 @@ function startSpeechRecognition(micButton) {
 // Creates and returns a mic button
 function createMicButton() {
     let micButton = document.createElement('button');
+    micButton.id = 'my-mic-button';
     let imageUrl = chrome.runtime.getURL("src/assets/mic-icon.png");
     micButton.style.backgroundImage = `url('${imageUrl}')`;
     micButton.style.backgroundRepeat = 'no-repeat';
@@ -37,6 +47,11 @@ chrome.runtime.onMessage.addListener((request) => {
     if (request.message === 'TabUpdated') {
         let textArea = document.getElementById('prompt-textarea');
         let parentElement = textArea.parentElement;
+
+        let existingMicButton = document.getElementById('my-mic-button');
+        if (existingMicButton) {
+            return
+        }
 
         let wrapperDiv = document.createElement('div');
         wrapperDiv.style.display = 'flex';
