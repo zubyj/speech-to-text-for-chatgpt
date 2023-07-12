cf57fe123bdd97757bb6d4111377efc8e3affc9c
Zuby Javed <zubyjaved@gmail.com>
2023-07-11 17:15:48 -0700

    only inject the button on the page if chat.openai.com is open and the page is finished loading

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete') {
+    if (changeInfo.status === 'complete' && tab.url.includes('chat.openai.com')) {
         chrome.tabs.sendMessage(tabId, { message: 'TabUpdated' });
     }
 });
