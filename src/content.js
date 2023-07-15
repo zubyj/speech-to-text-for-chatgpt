22eb75caa451204e97a9232fd7c55e49a7ed1d10
Zuby Javed <zubyjaved@gmail.com>
2023-07-15 12:33:56 -0700

    show the user their text to speech as they talk

--- src/content.js
+++ src/content.js
@@ -2,6 +2,8 @@
 function startSpeechRecognition(micButton) {
     let recognition = new webkitSpeechRecognition();
     recognition.lang = 'en-US';
+    recognition.interimResults = true;
+
     let textArea = document.getElementById('prompt-textarea');
     let timeout;
 
@@ -12,36 +14,23 @@ function startSpeechRecognition(micButton) {
     }
 
     recognition.onspeechstart = function () {
-        clearTimeout(timeout);  // Clear the timeout when speech starts
+        clearTimeout(timeout);
     }
 
     recognition.onspeechend = function () {
-        // Start a timeout to stop recognition after 5 seconds of no speech
-        timeout = setTimeout(() => {
-            recognition.stop();
-        }, 5000);
-    }
-
-    recognition.onresult = function (event) {
-        let transcript = event.results[0][0].transcript;
-        // get cursor position and selection if any
-        const selectionStart = textArea.selectionStart;
-        const selectionEnd = textArea.selectionEnd;
-        // insert transcript at cursor position
-        const value = textArea.value;
-        textArea.value = value.slice(0, selectionStart) + transcript + value.slice(selectionEnd);
-        // move cursor to end of inserted text
-        textArea.selectionStart = selectionStart + transcript.length;
-        textArea.selectionEnd = textArea.selectionStart;
-        // manually trigger input event
+        // Create and dispatch an input event
         const inputEvent = new Event('input', { bubbles: true });
         textArea.dispatchEvent(inputEvent);
-        micButton.classList.remove('active');  // Remove 'active' class
+        micButton.classList.remove('active');
         let newImageUrl = chrome.runtime.getURL("./src/assets/mic.png");
         micButton.style.backgroundImage = `url('${newImageUrl}')`;
         textArea.focus();
     }
 
+    recognition.onresult = function (event) {
+        let transcript = event.results[0][0].transcript;
+        textArea.value = transcript;
+    }
 
     recognition.start();
 
@@ -85,16 +74,6 @@ chrome.runtime.onMessage.addListener((request) => {
             const micButton = createMicButton();
             let textArea = document.getElementById('prompt-textarea');
 
-            // ...
-
-            // Add input event listener to textArea
-            textArea.addEventListener('input', () => {
-                // If user starts typing, hide the mic button
-                if (textArea.value !== '') {
-                    micButton.style.display = 'none';
-                }
-            });
-
             // Turn off speech recognition on enter (but not shift + enter)
             textArea.addEventListener('keydown', (e) => {
                 if (e.key === 'Enter' && !e.shiftKey) {
@@ -110,7 +89,6 @@ chrome.runtime.onMessage.addListener((request) => {
                 micButton.style.display = 'inline-block'; // Show the mic button
             });
 
-
             // Move the mic button and text area to a wrapper div
             let parentElement = textArea.parentElement;
             let wrapperDiv = document.createElement('div');
@@ -120,17 +98,6 @@ chrome.runtime.onMessage.addListener((request) => {
             wrapperDiv.appendChild(textArea);
             parentElement.appendChild(wrapperDiv);
             textArea.focus();
-
-            // Add input event listener to textArea
-            textArea.addEventListener('input', () => {
-                // If user starts typing, hide the mic button
-                if (textArea.value !== '') {
-                    micButton.style.display = 'none';
-                } else {
-                    // Show the mic button if textArea is empty
-                    micButton.style.display = 'inline-block';
-                }
-            });
         }, 500);
     }
 });
