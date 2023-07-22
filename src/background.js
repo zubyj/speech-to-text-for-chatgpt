34af11d3411221b16946e725335b94f7f4236ad4
Zuby Javed <zubyjaved@gmail.com>
2023-07-22 13:10:39 -0700

    remove tabs permission

--- src/background.js
+++
@@ -1,5 +0,0 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url.includes('chat.openai.com')) {
-        chrome.tabs.sendMessage(tabId, { message: 'TabUpdated' });
-    }
-});
--- src/content.js
+++ src/content.js
@@ -78,46 +78,56 @@ function createMicButton() {
     return micButton;
 }
 
-chrome.runtime.onMessage.addListener((request) => {
-    if (request.message === 'TabUpdated') {
-        setTimeout(() => {
-            if (document.getElementById('mic-button')) {
-                return;
-            }
-            fetch(chrome.runtime.getURL('./src/assets/styles.css'))
-                .then(response => response.text())
-                .then(data => {
-                    let style = document.createElement('style');
-                    style.innerHTML = data;
-                    document.head.appendChild(style);
-                });
-            const micButton = createMicButton();
-            let textArea = document.getElementById('prompt-textarea');
 
-            // Turn off speech recognition on enter (but not shift + enter)
-            textArea.addEventListener('keydown', (e) => {
-                if (e.key === 'Enter' && !e.shiftKey) {
-                    micButton.style.display = 'inline-block'; // Show the mic button
-                }
-            });
-
-            // Get the submit button
-            const chat = document.querySelector('textarea[tabindex="0"]');
-            const submitButton = chat.parentNode.querySelector('button:nth-child(2)');
-            // Turn off speech recognition when submit button is clicked
-            submitButton.addEventListener('click', () => {
-                micButton.style.display = 'inline-block'; // Show the mic button
-            });
-
-            // Move the mic button and text area to a wrapper div
-            let parentElement = textArea.parentElement;
-            let wrapperDiv = document.createElement('div');
-            wrapperDiv.classList.add('wrapper-div');
-            parentElement.removeChild(textArea);
-            wrapperDiv.appendChild(micButton);
-            wrapperDiv.appendChild(textArea);
-            parentElement.appendChild(wrapperDiv);
-            textArea.focus();
-        }, 100);
+async function main() {
+    // If the mic button already exists, don't do anything
+    let micButton = document.getElementById('mic-button');
+    if (micButton) {
+        return;
     }
-});
+    micButton = createMicButton();
+
+    // If the textArea doesnt exist, dont do anything 
+    let textArea = document.getElementById('prompt-textarea');
+    if (!textArea) {
+        return;
+    }
+
+
+    try {
+        const response = await fetch(chrome.runtime.getURL('./src/assets/styles.css'));
+        const data = await response.text();
+        let style = document.createElement('style');
+        style.innerHTML = data;
+        document.head.appendChild(style);
+    } catch (error) {
+        console.error('Error fetching CSS file:', error);
+    }
+
+    // Turn off speech recognition on enter (but not shift + enter)
+    textArea.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            micButton.style.display = 'inline-block'; // Show the mic button
+        }
+    });
+
+    // // Get the submit button
+    // const chat = document.querySelector('textarea[tabindex="0"]');
+    // const submitButton = chat.parentNode.querySelector('button:nth-child(2)');
+    // // Turn off speech recognition when submit button is clicked
+    // submitButton.addEventListener('click', () => {
+    //     micButton.style.display = 'inline-block'; // Show the mic button
+    // });
+
+    // Move the mic button and text area to a wrapper div
+    let parentElement = textArea.parentElement;
+    let wrapperDiv = document.createElement('div');
+    wrapperDiv.classList.add('wrapper-div');
+    parentElement.removeChild(textArea);
+    wrapperDiv.appendChild(micButton);
+    wrapperDiv.appendChild(textArea);
+    parentElement.appendChild(wrapperDiv);
+    textArea.focus();
+}
+
+main();
